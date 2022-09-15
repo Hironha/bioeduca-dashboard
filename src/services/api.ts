d360b5a412d4c5ca082b29cf55dfdb98a4963d22
hironha <rafaelhirokato@gmail.com>
2022-09-14 21:36:37 -0300

    :sparkles: criação de api e interceptors

---
+++ src/services/api.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { getAuth } from 'firebase/auth';
+
+export const api = axios.create({
+	baseURL: process.env.REACT_APP_API_URL,
+	timeout: 60 * 1000,
+	validateStatus: (status) => {
+		return status >= 200 && status <= 300;
+	},
+});
+
+api.interceptors.request.use(async (response) => {
+	if (response.headers) {
+		if (!response.headers['Authorization']) {
+			const token = await getAuth().currentUser?.getIdToken();
+			response.headers['Authorization'] = `Bearer ${token}`;
+		}
+	} else {
+		const token = await getAuth().currentUser?.getIdToken();
+		response.headers = {
+			Authorization: `Bearer ${token}`,
+		};
+	}
+
+	return Promise.resolve(response);
+});
+
+api.interceptors.response.use(
+	async (response) => response,
+	async (error) => {
+		const user = getAuth().currentUser;
+		if (error?.response?.data?.code === 403 && !error.config._retry && user) {
+			error.config._retry = true;
+			const token = await user.getIdToken(true);
+			api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+			return api(error.config);
+		}
+		return Promise.reject(error);
+	}
+);
