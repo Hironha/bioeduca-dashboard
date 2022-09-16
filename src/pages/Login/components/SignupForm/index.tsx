eea440b5d70d0b1c2e6db20c90c126b63c612d50
Kobilacz <luizgustavokobilacz@gmail.com>
2022-09-15 23:35:09 -0300

    :sparkles: Validação dos campos de criar conta

--- src/pages/Login/components/SignupForm/index.tsx
+++ src/pages/Login/components/SignupForm/index.tsx
@@ -57,19 +57,63 @@ export const SignupForm = () => {
 			>
 				<FormRow>
 					<Col span={24}>
-						<Form.Item label="Email" name={SignupFormInputs.EMAIL}>
+						<Form.Item
+							label="Email"
+							name={SignupFormInputs.EMAIL}
+							rules={[
+								{
+									required: true,
+									message: 'Insira seu email!',
+								},
+								{
+									type: 'email',
+									message: 'Insira um email válido!',
+								},
+							]}
+						>
 							<Input placeholder="youremail@example.com" />
 						</Form.Item>
 					</Col>
 
 					<Col span={24}>
-						<Form.Item label="Senha" name={SignupFormInputs.PASSWORD}>
+						<Form.Item
+							label="Senha"
+							name={SignupFormInputs.PASSWORD}
+							rules={[
+								{
+									required: true,
+									message: 'Insira sua senha!',
+								},
+								{
+									min: 8,
+									message: 'Sua senha deve conter, no mínimo, 8 caracteres!',
+								},
+							]}
+						>
 							<Input.Password iconRender={(visible) => (visible ? <UnlockIcon /> : <LockIcon />)} />
 						</Form.Item>
 					</Col>
 
 					<Col span={24}>
-						<Form.Item label="Confirmação de senha" name={SignupFormInputs.PASSWORD_CONFIRM}>
+						<Form.Item
+							label="Confirmação de senha"
+							name={SignupFormInputs.PASSWORD_CONFIRM}
+							dependencies={[SignupFormInputs.PASSWORD]}
+							rules={[
+								{
+									required: true,
+									message: 'Confirme sua senha!',
+								},
+								({ getFieldValue }) => ({
+									validator(_, value) {
+										if (!value || getFieldValue(SignupFormInputs.PASSWORD) == value) {
+											return Promise.resolve();
+										}
+										return Promise.reject('As senhas devem ser iguais!');
+									},
+								}),
+							]}
+						>
 							<Input.Password iconRender={(visible) => (visible ? <UnlockIcon /> : <LockIcon />)} />
 						</Form.Item>
 					</Col>
