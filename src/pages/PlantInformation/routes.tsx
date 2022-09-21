0f4a05a4b363cd89e11cbbc52447bc8ea78ab810
Kobilacz <luizgustavokobilacz@gmail.com>
2022-09-21 15:20:08 -0300

    :sparkles: Adição de redirecionamento

--- src/pages/PlantInformation/routes.tsx
+++ src/pages/PlantInformation/routes.tsx
@@ -1,16 +1,24 @@
+import { useContext } from 'react';
 import { Outlet, type RouteObject } from 'react-router-dom';
+
+import { Redirect } from '@components/Redirect';
+import { AuthContext } from '@providers/AuthProvider';
 
 import { PageLayout } from '@components/Layout';
 import { CreatePlantInformationPage } from './components/CreatePlantInformationPage';
-import { ListPlantInformationsPage } from './components/ListPlantInformationsPage'
+import { ListPlantInformationsPage } from './components/ListPlantInformationsPage';
 
 export const usePlantInformationRoutes = (): RouteObject[] => {
+	const { verifyAuthentication } = useContext(AuthContext);
+
 	return [
 		{
 			element: (
-				<PageLayout>
-					<Outlet />
-				</PageLayout>
+				<Redirect to="login" validation={!verifyAuthentication()}>
+					<PageLayout>
+						<Outlet />
+					</PageLayout>
+				</Redirect>
 			),
 			children: [
 				{ path: '/plant-informations', element: <ListPlantInformationsPage /> },
