35f1f544d063c29c3f74524b8cffa118156d8acf
Kobilacz <luizgustavokobilacz@gmail.com>
2022-09-21 14:49:31 -0300

    :lipstick: Link para página de criação de informação

--- src/pages/PlantInformation/components/ListPlantInformationsPage/index.tsx
+++ src/pages/PlantInformation/components/ListPlantInformationsPage/index.tsx
@@ -1,6 +1,6 @@
 import { Content } from '@components/Content';
 import { ListPlantInformations } from '../ListPlantInformations';
-import { DeletePlantInformationAlert } from './styles';
+import { DeletePlantInformationAlert, CreatePlantInformationLink } from './styles';
 
 export const ListPlantInformationsPage = () => {
 	return (
@@ -8,15 +8,22 @@ export const ListPlantInformationsPage = () => {
 			<Content.Header level={3}>Informações das Plantas</Content.Header>
 			<Content.Description>
 				Informações das plantas são todas as informações que uma planta pode possuir. Elas são
-				gerenciadas pelo usuário, onde o mesmo pode cadastrar, editar ou excuir uma informação. Ao
-				cadastrar uma nova informação, as plantas poderão ter essa informação vinculada a elas, por
-				exemplo, caso cadastre a informação <strong>Bioma</strong>, as plantas poderão ter o campo{' '}
-				<strong>Bioma</strong> com valores distintos para cada uma.
+				gerenciadas pelo usuário, onde o mesmo pode{' '}
+				<CreatePlantInformationLink
+					to={'/plant-informations/create'}
+					style={{ textDecoration: 'underline' }}
+				>
+					cadastrar
+				</CreatePlantInformationLink>
+				, editar ou excuir uma informação. Ao cadastrar uma nova informação, as plantas poderão ter
+				essa informação vinculada a elas, por exemplo, caso cadastre a informação{' '}
+				<strong>Bioma</strong>, as plantas poderão ter o campo <strong>Bioma</strong> com valores
+				distintos para cada uma.
 			</Content.Description>
 			<DeletePlantInformationAlert
 				showIcon
 				type="info"
-				description="A exclusão de uma informação das plantas acarretará na exclusão dessas informações nas plantas. Por exemplo, ao excluir o campo Bioma, o mesmo será removido de todas as plantas que possuem esse campo."
+				description="Excluir uma informação das plantas faz com que ela seja apagada de todas as plantas cadastradas. Por exemplo, ao excluir o campo Bioma, o mesmo será removido de todas as plantas que possuem esse campo."
 			/>
 			<Content.Body>
 				<ListPlantInformations />
--- src/pages/PlantInformation/components/ListPlantInformationsPage/styles.ts
+++ src/pages/PlantInformation/components/ListPlantInformationsPage/styles.ts
@@ -1,6 +1,12 @@
+import { Link } from 'react-router-dom';
 import { Alert } from 'antd';
 import styled from 'styled-components';
 
 export const DeletePlantInformationAlert = styled(Alert)`
 	margin: 12px 0;
+`;
+
+export const CreatePlantInformationLink = styled(Link)`
+	color: ${(props) => props.theme.colors.success} !important;
+	font-weight: bold;
 `;
