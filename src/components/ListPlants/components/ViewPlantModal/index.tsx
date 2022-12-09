b754c8bcf948dd183b6796cd76c33587c47fa9f6
hironha <rafaelhirokato@gmail.com>
2022-12-08 22:18:26 -0300

    :recycle: add uso das props de typography

--- src/components/ListPlants/components/ViewPlantModal/index.tsx
+++ src/components/ListPlants/components/ViewPlantModal/index.tsx
@@ -14,87 +14,89 @@ export const ViewPlantModal = ({ plantId, visible, ...props }: ViewPlantModalPro
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Nome Popular</Typography>
-			<Typography>Ipê-rosa</Typography>
+			<Typography.Paragraph strong>Nome Popular</Typography.Paragraph>
+			<Typography.Paragraph>Ipê-rosa</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Nome Científico</Typography>
-			<Typography style={{ fontStyle: 'italic' }}>Handroanthus impetiginosus</Typography>
+			<Typography.Paragraph strong>Nome Científico</Typography.Paragraph>
+			<Typography.Paragraph italic>Handroanthus impetiginosus</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Bioma</Typography>
-			<Typography>Amazônia, Caatinga, Cerrado, Mata Atlântica e Pantanal</Typography>
+			<Typography.Paragraph strong>Bioma</Typography.Paragraph>
+			<Typography.Paragraph>
+				Amazônia, Caatinga, Cerrado, Mata Atlântica e Pantanal
+			</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Época de floração</Typography>
-			<Typography>Julho a agosto</Typography>
+			<Typography.Paragraph strong>Época de floração</Typography.Paragraph>
+			<Typography.Paragraph>Julho a agosto</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Época de frutificação</Typography>
-			<Typography>Setembro a outubro</Typography>
+			<Typography.Paragraph strong>Época de frutificação</Typography.Paragraph>
+			<Typography.Paragraph>Setembro a outubro</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Família</Typography>
-			<Typography>BIGNONIACEAE</Typography>
+			<Typography.Paragraph strong>Família</Typography.Paragraph>
+			<Typography.Paragraph>BIGNONIACEAE</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Filotaxia</Typography>
-			<Typography>Oposta</Typography>
+			<Typography.Paragraph strong>Filotaxia</Typography.Paragraph>
+			<Typography.Paragraph>Oposta</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Grupo ecológico</Typography>
-			<Typography>Secundária</Typography>
+			<Typography.Paragraph strong>Grupo ecológico</Typography.Paragraph>
+			<Typography.Paragraph>Secundária</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Ocorrência</Typography>
-			<Typography>
+			<Typography.Paragraph strong>Ocorrência</Typography.Paragraph>
+			<Typography.Paragraph>
 				Distrito Federal (DF), Goiás (GO), Mato Grosso do Sul (MS), Mato Grosso (MT), Alagoas (AL),
 				Bahia (BA), Ceará (CE), Maranhão (MA), Pará (PA), Pernambuco (PE), Piauí (PI), Rio Grande do
 				Norte (RN), Sergipe (SE), Acre (AC), Pará (PA), Rondônia (RO), Tocantins (TO), Espírito
 				Santo (ES), Minas Gerais (MG), Rio de Janeiro (RJ) e São Paulo (SP)
-			</Typography>
+			</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Origem</Typography>
-			<Typography>Nativa</Typography>
+			<Typography.Paragraph strong>Origem</Typography.Paragraph>
+			<Typography.Paragraph>Nativa</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Síndrome de disperção</Typography>
-			<Typography>Anemocoria</Typography>
+			<Typography.Paragraph strong>Síndrome de disperção</Typography.Paragraph>
+			<Typography.Paragraph>Anemocoria</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Síndrome de polinização</Typography>
-			<Typography>Monóica</Typography>
+			<Typography.Paragraph strong>Síndrome de polinização</Typography.Paragraph>
+			<Typography.Paragraph>Monóica</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Tipo de folha</Typography>
-			<Typography>Folha composta</Typography>
+			<Typography.Paragraph strong>Tipo de folha</Typography.Paragraph>
+			<Typography.Paragraph>Folha composta</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Tipo de fruto</Typography>
-			<Typography>Síliqua</Typography>
+			<Typography.Paragraph strong>Tipo de fruto</Typography.Paragraph>
+			<Typography.Paragraph>Síliqua</Typography.Paragraph>
 
 			<Divider />
 
-			<Typography style={{ fontWeight: 'bold' }}>Utilizações</Typography>
-			<Typography>
+			<Typography.Paragraph strong>Utilizações</Typography.Paragraph>
+			<Typography.Paragraph>
 				Espécie muito utilizada na arborização urbana devido à beleza de suas flores. Sua madeira é
 				utilizada para construção civil, postes e dormentes. Espécie indicada para a restauração de
 				ecossistemas degradados
-			</Typography>
+			</Typography.Paragraph>
 
 			<Divider />
 		</Modal>
