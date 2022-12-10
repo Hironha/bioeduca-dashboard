150e3e3c6ab625a5ff41121b72f014cf69fcd0f4
Kobilacz <luizgustavokobilacz@gmail.com>
2022-12-10 11:03:47 -0300

    :lipstick: Ajuste no modal de visualização de dados das plantas

--- src/components/ListPlants/components/ViewPlantModal/index.tsx
+++ src/components/ListPlants/components/ViewPlantModal/index.tsx
@@ -1,7 +1,10 @@
-import { Modal, Typography, Divider, type ModalProps } from 'antd';
-import { ModalTitle } from './styles';
+import { Modal, Typography, Divider, Carousel, type ModalProps } from 'antd';
+import { ModalTitle, LoadingContainer } from './styles';
+import { Loading } from '@components/Loading';
 
 import { useConsultPlant } from '@services/hooks/plant/useConsultPlant';
+import { Fragment, useMemo, useCallback } from 'react';
+
 export type ViewPlantModalProps = Omit<ModalProps, 'footer' | 'children'> & {
 	plantId?: string;
 };
@@ -20,97 +23,65 @@ export const ViewPlantModal = ({ plantId, visible, ...props }: ViewPlantModalPro
 		},
 	});
 
+	const normalizeString = useCallback((str: string) => {
+		return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+	}, []);
+
+	const plantData = useMemo(() => {
+		return Object.entries(consultPlantResult.data?.additional_informations || []).sort(
+			([nameLeft], [nameRight]) => {
+				const normalizedNameLeft = normalizeString(nameLeft);
+				const normalizedNameRight = normalizeString(nameRight);
+				if (normalizedNameLeft > normalizedNameRight) return 1;
+				if (normalizedNameLeft < normalizedNameRight) return -1;
+				return 0;
+			}
+		);
+	}, [consultPlantResult.data?.additional_informations, normalizeString]);
+
 	return (
 		<Modal {...props} visible={isVisible} footer={null}>
-			<ModalTitle>Dados da planta</ModalTitle>
+			{consultPlantResult.isLoading ? (
+				<LoadingContainer>
+					<Loading />
+				</LoadingContainer>
+			) : (
+				<>
+					<ModalTitle>Dados da planta</ModalTitle>
 
-			<Divider />
+					<Divider />
 
-			<Typography.Paragraph strong>Nome Popular</Typography.Paragraph>
-			<Typography.Paragraph>Ipê-rosa</Typography.Paragraph>
+					<Carousel dotPosition="top" draggable>
+						{consultPlantResult.data?.images.map((image) => {
+							return <img src={image} />;
+						})}
+					</Carousel>
 
-			<Divider />
+					<Divider />
 
-			<Typography.Paragraph strong>Nome Científico</Typography.Paragraph>
-			<Typography.Paragraph italic>Handroanthus impetiginosus</Typography.Paragraph>
+					<Typography.Text strong>Nome Popular</Typography.Text>
+					<Typography.Paragraph>{consultPlantResult.data?.popular_name}</Typography.Paragraph>
 
-			<Divider />
+					<Divider />
 
-			<Typography.Paragraph strong>Bioma</Typography.Paragraph>
-			<Typography.Paragraph>
-				Amazônia, Caatinga, Cerrado, Mata Atlântica e Pantanal
-			</Typography.Paragraph>
+					<Typography.Text strong>Nome Científico</Typography.Text>
+					<Typography.Paragraph italic>
+						{consultPlantResult.data?.scientific_name}
+					</Typography.Paragraph>
 
-			<Divider />
+					<Divider />
 
-			<Typography.Paragraph strong>Época de floração</Typography.Paragraph>
-			<Typography.Paragraph>Julho a agosto</Typography.Paragraph>
-
-			<Divider />
-
-			<Typography.Paragraph strong>Época de frutificação</Typography.Paragraph>
-			<Typography.Paragraph>Setembro a outubro</Typography.Paragraph>
-
-			<Divider />
-
-			<Typography.Paragraph strong>Família</Typography.Paragraph>
-			<Typography.Paragraph>BIGNONIACEAE</Typography.Paragraph>
-
-			<Divider />
-
-			<Typography.Paragraph strong>Filotaxia</Typography.Paragraph>
-			<Typography.Paragraph>Oposta</Typography.Paragraph>
-
-			<Divider />
-
-			<Typography.Paragraph strong>Grupo ecológico</Typography.Paragraph>
-			<Typography.Paragraph>Secundária</Typography.Paragraph>
-
-			<Divider />
-
-			<Typography.Paragraph strong>Ocorrência</Typography.Paragraph>
-			<Typography.Paragraph>
-				Distrito Federal (DF), Goiás (GO), Mato Grosso do Sul (MS), Mato Grosso (MT), Alagoas (AL),
-				Bahia (BA), Ceará (CE), Maranhão (MA), Pará (PA), Pernambuco (PE), Piauí (PI), Rio Grande do
-				Norte (RN), Sergipe (SE), Acre (AC), Pará (PA), Rondônia (RO), Tocantins (TO), Espírito
-				Santo (ES), Minas Gerais (MG), Rio de Janeiro (RJ) e São Paulo (SP)
-			</Typography.Paragraph>
-
-			<Divider />
-
-			<Typography.Paragraph strong>Origem</Typography.Paragraph>
-			<Typography.Paragraph>Nativa</Typography.Paragraph>
-
-			<Divider />
-
-			<Typography.Paragraph strong>Síndrome de disperção</Typography.Paragraph>
-			<Typography.Paragraph>Anemocoria</Typography.Paragraph>
-
-			<Divider />
-
-			<Typography.Paragraph strong>Síndrome de polinização</Typography.Paragraph>
-			<Typography.Paragraph>Monóica</Typography.Paragraph>
-
-			<Divider />
-
-			<Typography.Paragraph strong>Tipo de folha</Typography.Paragraph>
-			<Typography.Paragraph>Folha composta</Typography.Paragraph>
-
-			<Divider />
-
-			<Typography.Paragraph strong>Tipo de fruto</Typography.Paragraph>
-			<Typography.Paragraph>Síliqua</Typography.Paragraph>
-
-			<Divider />
-
-			<Typography.Paragraph strong>Utilizações</Typography.Paragraph>
-			<Typography.Paragraph>
-				Espécie muito utilizada na arborização urbana devido à beleza de suas flores. Sua madeira é
-				utilizada para construção civil, postes e dormentes. Espécie indicada para a restauração de
-				ecossistemas degradados
-			</Typography.Paragraph>
-
-			<Divider />
+					{plantData.map(([key, value]) => {
+						return (
+							<Fragment key={key}>
+								<Typography.Text strong>{key}</Typography.Text>
+								<Typography.Paragraph>{value}</Typography.Paragraph>
+								<Divider />
+							</Fragment>
+						);
+					})}
+				</>
+			)}
 		</Modal>
 	);
 };
--- src/components/ListPlants/components/ViewPlantModal/styles.ts
+++ src/components/ListPlants/components/ViewPlantModal/styles.ts
@@ -4,3 +4,9 @@ import { Typography } from 'antd';
 export const ModalTitle = styled(Typography.Title).attrs(() => ({ level: 4 }))`
 	text-align: center;
 `;
+
+export const LoadingContainer = styled.div`
+	width: 100%;
+	justify-content: center;
+	align-items: center;
+`;
